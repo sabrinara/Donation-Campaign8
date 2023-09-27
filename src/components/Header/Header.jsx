67cb08a99af2e5a55ead4ae60e71489a4f2b2e54
sabrinara <you@example.com>
2023-09-27 23:16:21 +0600

    Search Button Modified

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,37 +1,42 @@
 
 // import Navbar from "../Navbar/Navbar";
 
-const Header = () => {
+const Header = ({ searchData, handleSearch, setSearchData }) => {
+
 
     return (
         <div className="hero h-[70vh]" style={{ backgroundImage: 'url(https://i.ibb.co/myPZxYg/cover.png)' }}>
             <div className="hero-overlay bg-opacity-80 bg-slate-100 ">
-               {/* <Navbar></Navbar> */}
+                {/* <Navbar></Navbar> */}
             </div>
             <div className="hero-content text-center text-neutral-content">
                 <div className="max-w-lg">
                     <h1 className="text-3xl font-extrabold text-black">I Grow By Helping People In Need</h1>
                     <div className="flex my-4 justify-center ">
                         <div className="border rounded-md">
-                            <input
-                                type="text"
+                          <form action="">
+                          <input
+                                value={searchData}
+                                onChange={(e) => setSearchData(e.target.value)}
+                                type="search"
+                                id="search"
                                 placeholder="Search Now"
                                 className="px-4 py-2 bg-transparent 
                                  text-black"
                             />
 
-                            <button
+                            <button  onClick={handleSearch} type="button"
                                 className="text-white font-bold bg-[#FF444A] px-4 py-2  rounded-none"
-                            >
-                                Search
+                            > Search
                             </button>
+                          </form>
                         </div>
                     </div>
 
                 </div>
             </div>
         </div>
-       
+
     );
 };
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,15 +2,41 @@
 import { useLoaderData } from "react-router-dom";
 import DonationItems from "../../components/DonationItems/DonationItems";
 import Header from "../../components/Header/Header";
-
-
+import { useEffect, useState } from "react";
 
 const Home = () => {
-    const donations = useLoaderData();
+    // const donations = useLoaderData();
+
+    const [data, setData] = useState([]);
+    const [filteredData, setFilteredData] = useState([]);
+    const [searchData, setSearchData] = useState('');
+
+    useEffect(() => {
+
+        fetch('./data.json')
+            .then((response) => response.json())
+            .then((data) => {
+                setData(data);
+                setFilteredData(data);
+            })
+            .catch((error) => console.error(error));
+    }, []);
+
+    const handleSearch = () => {
+
+        if (!searchData) {
+            setFilteredData(data);
+        } else {
+            const filtered = data.filter((item) => item.category.toLowerCase().includes(searchData.toLowerCase()))
+
+            console.log(filtered);
+            setFilteredData(filtered);
+        }
+    };
     return (
         <div>
-            <Header></Header>
-            <DonationItems donations = {donations}></DonationItems>
+            <Header handleSearch={handleSearch} setSearchData={setSearchData} searchData={searchData} ></Header>
+            <DonationItems donations={filteredData}></DonationItems>
         </div>
     );
 };
